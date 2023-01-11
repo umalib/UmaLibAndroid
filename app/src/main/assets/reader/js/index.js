dbc6c12944b45524bcbdda5538bc3f812ac5dd41
WindyWisper <umalib629@outlook.com>
2023-01-11 09:02:53 +0800

    build: initialize branch version

--- app/src/main/assets/reader/js/index.js
+++
@@ -1,8 +0,0 @@
-/**
-* 统一管理js调用安卓方法
-* @param method 方法名
-* @param params 参数 数组格式
-*/
-var callAndroidMethod = function(method, params){
-    window.jsInterface.invokeMethod(method, [JSON.stringify(params)]);//json对象转成字符串，再转成字符串数组
-}
