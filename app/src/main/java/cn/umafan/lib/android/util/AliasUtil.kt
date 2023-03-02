955db205b1aba0fae833e6c8f12b2f74c200ab1d
WindyWisper <umalib629@outlook.com>
2023-03-02 17:05:48 +0800

    feat: update alias in data.js

--- app/src/main/java/cn/umafan/lib/android/util/AliasUtil.kt
+++ app/src/main/java/cn/umafan/lib/android/util/AliasUtil.kt
@@ -6,12 +6,15 @@ object AliasUtil {
     init {
         aliasMap["东海帝王"] = "东海帝皇"
         aliasMap["东瀛佐敦"] = "岛川乔登"
+        aliasMap["光辉致意"] = "Light Hello"
         aliasMap["名将怒涛"] = "名将户仁"
         aliasMap["名竞应援"] = "齐叫好"
         aliasMap["圣王光环"] = "帝王光辉/帝皇光辉"
         aliasMap["大拓太阳神"] = "大德太阳"
         aliasMap["安身立命"] = "幸存者"
+        aliasMap["小小蚕茧"] = "小巧圆茧/Little Cocon"
         aliasMap["待兼诗歌剧"] = "待兼唐怀瑟"
+        aliasMap["快乐米可"] = "快乐温顺/Happy Mik"
         aliasMap["成田白仁"] = "成田拜仁"
         aliasMap["成田路"] = "成田速王"
         aliasMap["摩耶重炮"] = "重炮"
@@ -21,8 +24,10 @@ object AliasUtil {
         aliasMap["目白赖恩"] = "目白莱恩/目白雷恩"
         aliasMap["第一红宝石"] = "第一红宝"
         aliasMap["艾尼斯风神"] = "艾尼风神"
+        aliasMap["苦味糖衣"] = "微苦糖渍/Bitter Glasse"
         aliasMap["菱亚马逊"] = "亚马逊"
         aliasMap["超级小海湾"] = "超级小溪/超级溪流"
+        aliasMap["里见女尊"] = "里见蕾娜斯"
         aliasMap["鲁铎象征"] = "鲁道夫象征"
     }
 
