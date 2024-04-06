61f3900ff09cede812b7d6d151e140da63b53bbb
ForeverDdB <835236331@qq.com>
2024-04-06 21:12:08 +0800

    fix: fix network error

--- app/src/main/java/cn/umafan/lib/android/util/network/ServiceCreator.kt
+++ app/src/main/java/cn/umafan/lib/android/util/network/ServiceCreator.kt
@@ -1,16 +1,54 @@
 package cn.umafan.lib.android.util.network
 
+import android.annotation.SuppressLint
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.net.Proxy
+import java.security.SecureRandom
+import java.security.cert.*
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
 
 object ServiceCreator {
+
+    private fun createSSLSocketFactory(): SSLSocketFactory? {
+        var ssfFactory: SSLSocketFactory? = null
+        try {
+            val mMyTrustManager = MyTrustManager()
+            val sc = SSLContext.getInstance("TLS")
+            sc.init(null, arrayOf<TrustManager>(mMyTrustManager), SecureRandom())
+            ssfFactory = sc.socketFactory
+        } catch (ignored: Exception) {
+            ignored.printStackTrace()
+        }
+        return ssfFactory
+    }
+
+    //实现X509TrustManager接口
+    @SuppressLint("CustomX509TrustManager")
+    class MyTrustManager : X509TrustManager {
+        @SuppressLint("TrustAllX509TrustManager")
+        @Throws(CertificateException::class)
+        override fun checkClientTrusted(chain: Array<X509Certificate?>?, authType: String?) {}
+        @SuppressLint("TrustAllX509TrustManager")
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(chain: Array<X509Certificate?>?, authType: String?) {}
+
+        override fun getAcceptedIssuers(): Array<X509Certificate> {
+            return arrayOf()
+        }
+    }
+
 
     private const val BASE_URL = "https://umalib.gitgud.site/"
 
     private val okHttpClient = OkHttpClient.Builder()
         .proxy(Proxy.NO_PROXY)
+        .sslSocketFactory(createSSLSocketFactory()!!, MyTrustManager())
         .build()
 
     private val retrofit: Retrofit = Retrofit.Builder()
